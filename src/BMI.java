1389dd48218724d1e638d18df222d39cb133d9c5
Aleksei <Aleksei_Lekomtsev@epam.com>
2020-11-26 17:41:37 +0300

    Refactoring

--- src/BMI.java
+++ src/BMI.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 
 public class BMI {
     private String username;
-    private double i;
+    private double index;
 
     public void init() {
         Scanner scanner = new Scanner(System.in);
+
         print("What is username?");
         username = scanner.nextLine();
         print("The username is " + username + ".");
         print("What is height of " + username + " in metres?");
+
         double height = scanner.nextDouble();
         while(height < 1.50 || height > 2.00) {
             printErr("The height must be between 1.50 and 2.00 metres.");
             height = scanner.nextDouble();
         }
         print("The height is " + height + ".");
+
         print("What is weight of " + username + " in kilograms?");
         double weight = scanner.nextDouble();
         while (weight < 45 || weight > 117) {
@@ -23,18 +26,19 @@ public class BMI {
             weight = scanner.nextDouble();
         }
         print("The weight is " + weight + ".");
-        i = weight / Math.pow(height, 2);
+
+        index = weight / Math.pow(height, 2);
     }
 
     public void print() {
         String result;
-        if (i < 18.5) {
+        if (index < 18.5) {
             result = "Underweight.";
-        } else if (i >= 18.5 && i < 25) {
+        } else if (index >= 18.5 && index < 25) {
             result = "Normal weight.";
-        } else if (i >= 25 && i < 30) {
+        } else if (index >= 25 && index < 30) {
             result = "Overweight.";
-        } else if (i >= 30 && i < 35) {
+        } else if (index >= 30 && index < 35) {
             result = "Obese.";
         } else {
             result = "Extremely obese.";
